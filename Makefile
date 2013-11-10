cf879cb6bb5502c5239772303555726af2ad4557
Pushpendre Rastogi <pushpendre@gmail.com>
2013-11-10 12:25:43 -0500

    first stab at predicate features, didn't get very far just addded a bunch of stuff to the extract_predicate_features.py file

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-#Check http://web.mit.edu/gnu/doc/html/make_10.html#SEC94 for explanation
-#run make train.sdp and then maketrain_predicate.features
+#Check http://web.mit.edu/gnu/doc/html/make_10.html#SEC94 for explanation of these make symbols
+#run make train.sdp and then make train_predicate.features
 
 train.csv test.csv : marneffe_data.csv
 	python split_into_train_test.py marneffe_data.csv
@@ -10,7 +10,7 @@ train.csv test.csv : marneffe_data.csv
 %.sdp : %.sent 
 	java -cp /Users/pushpendrerastogi/stanford-corenlp-full-2013-06-20/stanford-corenlp-3.2.0.jar:/Users/pushpendrerastogi/stanford-corenlp-full-2013-06-20/stanford-corenlp-3.2.0-models.jar -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,parse -outputExtension ".out" -file  $< && cp $<.out $@
 
-%_predicate.features : %.sdp %.csv #TODO
+%_predicate.features : %.sdp %.csv 
 	python extract_predicate_features.py $^ $@
 
 train_loglinear_model : train_predicate.features
--- extract_predicate_features.py
+++ extract_predicate_features.py
@@ -1,3 +1,39 @@
 ##
 #Create Predicate features by using the stanfor dependency parses and the event introducing verbs etc.
 ##
+import sys, csv
+stanford_parse_file_name=sys.argv[1]
+complete_csv_file_name=sys.argv[2]
+output_file_name=sys.argv[3]
+d=csv.DictReader(open(stanford_parse_file_name))
+ld=list(d)
+for e in ld:
+    f.write(e["normalization"])
+
+
+#| Judge | eText    | normalization                                                | CT_plus | CT_minus | PR_plus | PR_minus | PS_plus | PS_minus | Uu |
+#| Readr | agree    | Justices Sandra Day O'Connor and Anthony Kennedy agree       |       1 |        0 |       8 |        0 |       0 |        0 |  1 |
+#|       |          | that the law amounted to unconstitutional sex discrimination |         |          |         |          |         |          |    |
+#| Readr | hint     | there was no hint of trouble in the last conversation        |      10 |        0 |       0 |        0 |       0 |        0 |  0 |
+#| Readr | arrests  | there have been arrests in the slayings                      |       0 |       10 |       0 |        0 |       0 |        0 |  0 |
+#| Readr | amounted | the law amounted to unconstitutional sex discrimination      |       3 |        0 |       4 |        0 |       2 |        0 |  1 |
+#| Readr | raised   | the issues had been raised before                            |       0 |       10 |       0 |        0 |       0 |        0 |  0 |
+
+
+
+#*Predicate classes*
+#Sauri's thesis defines classes of predicates(nouns and verbs) that project the same veridicality value onto the events they introduce. (Appendix A, P#Age 164)
+#The classes also define the grammatical relations that need to hold between the predicate and the event it introduces, because grammatical contexts
+#matter for veridicality.
+#
+#Different veridicality values will indeed be assigned to X in He doesnt know that X and in He doesnt know if X.
+#The classes have names like ANNOUNCE, CONFIRM, CONJECTURE, and SAY.
+#We used dependency graphs produced by the Stanford parser to follow the path from the target event to the root of the sentence.
+#
+#If a predicate in the path was contained in one of the classes and the grammatical relation matched, 
+#      we added 
+#         1. the lemma of the predicate as a feature
+#         2. a feature marking the predicate class.
+
+
+
